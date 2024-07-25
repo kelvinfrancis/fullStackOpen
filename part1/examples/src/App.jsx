b9b1e7d52dfb54a3022ffcdf12677da1541ff57e
kelvinfrancis <kelvinmoquete0597@gmail.com>
2024-07-25 16:53:21 -0400

    Manejo de arrays - pendiente

--- part1/examples/src/App.jsx
+++ part1/examples/src/App.jsx
@@ -11,24 +11,53 @@ import { useState } from "react"
 //     </div>
 //   )
 // }
+// const Display = ({counter}) => <div>{counter}</div>
+
+// const Button = ({onSmash, text}) => (
+//   <button onClick={onSmash}>
+//     {text}
+//   </button>
+// )
 
 const App = () => {
   // Hook useState
-  const [ counter, setCounter ] = useState(0)
+  // const [ counter, setCounter ] = useState(0)
+  // console.log('rendering with counter value', counter)
 
-  // controladores de eventos
-  const increaseByOne = () => setCounter(counter + 1)
-  const setToZero = () => setCounter(0)
+  // // controladores de eventos
+  // const increaseByOne = () => {
+  //   console.log('increasing, value before', counter)
+  //   setCounter(counter + 1)
+  // }
+  // const decreaseByOne = () => {
+  //   console.log('decreasing, value before', counter)
+  //   setCounter(counter - 1)
+  // }
+  // const setToZero = () => {
+  //   console.log('resetting to zero, value before', counter)
+  //   setCounter(0)
+  // }
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+  
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        Plus
-      </button>
-      <button onClick={setToZero}>
-        Zero
-      </button>
+      {left}
+      <button onClick={handleLeftClick}>left </button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
