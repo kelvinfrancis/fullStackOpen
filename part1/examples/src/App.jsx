dd67e4ac3b8ced4a5e65b166cbfc25172e2ceb2b
kelvinfrancis <kelvinmoquete0597@gmail.com>
2024-07-27 00:13:19 -0400

    No definir componentes dentro de los componentes - Completed

--- part1/examples/src/App.jsx
+++ part1/examples/src/App.jsx
@@ -1,70 +1,30 @@
 import { useState } from "react"
-// const Hello = ({name, age}) => {
-//   const bornYear = () => new Date().getFullYear - age
+// Este es lugar correcto para definir un componente
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-//   return (
-//     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-//       <p>So you were probably born in {bornYear()}</p>
-//     </div>
-//   )
-// }
-// const Display = ({counter}) => <div>{counter}</div>
-
-// const Button = ({onSmash, text}) => (
-//   <button onClick={onSmash}>
-//     {text}
-//   </button>
-// )
+// No definas componentes adentro de otro componente
+const Display = props => <div>{props.value}</div>
 
 const App = () => {
   const [value, setValue] = useState(10)
-  // funcion que devuelve otra funcion (promesas)
-  const setToValue = (newValue) => () => {
+
+  const setToValue = newValue => {
     console.log('value now', newValue)
     setValue(newValue)
   }
-  // Hook useState
-  // const [ counter, setCounter ] = useState(0)
-  // console.log('rendering with counter value', counter)
 
-  // // controladores de eventos
-  // const increaseByOne = () => {
-  //   console.log('increasing, value before', counter)
-  //   setCounter(counter + 1)
-  // }
-  // const decreaseByOne = () => {
-  //   console.log('decreasing, value before', counter)
-  //   setCounter(counter - 1)
-  // }
-  // const setToZero = () => {
-  //   console.log('resetting to zero, value before', counter)
-  //   setCounter(0)
-  // }
-  // const [left, setLeft] = useState(0)
-  // const [right, setRight] = useState(0)
-  // const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-  
-  // const handleRightClick = () => {
-  //   setAll(allClicks.concat('R'))
-  //   setRight(right + 1)
-  // }
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left </button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
 
-export default App
+export default App 
